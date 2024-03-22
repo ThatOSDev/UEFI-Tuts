9970303deb925717227e90c1a2df5806d7037c51
ThatOSDev <100424906+ThatOSDev@users.noreply.github.com>
2024-03-21 17:15:47 -0700

    Update efilibs.h
    FIXED strcmp function.

--- src/tutorial 30/efilibs.h
+++ src/tutorial 30/efilibs.h
@@ -74,9 +74,10 @@ int strcmp(const char* a, const char* b)
 	int length = strlen(a);
 	for(int i = 0; i < length; i++)
 	{
-		if(a[i] != b[i]){return 0;}
+		if(a[i] < b[i]){return -1;}
+		if(a[i] > b[i]){return  1;}
 	}
-	return 1;
+	return 0;
 }
 
 typedef struct BLOCKINFO
